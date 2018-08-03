77a674fcbaac40721107802f263e32bf657e8977
Julian Einspenner <julianeinspenner@yahoo.de>
2018-08-03 07:57:43 +0200

    upload, refresh, design, much more

--- skripte.js
+++ skripte.js
@@ -37,7 +37,7 @@
         ip = window.location.hostname;
 		getImageInfoLink = "http://" + ip + ":4434/bootstrap/v1/images/" + userName;
 		uploadLink       = "http://" + ip + ":4434/bootstrap/v1/images/" + userName;
-		deletionLink     = "http://" + ip + ":4434/bootstrap/v1/images/" + userName; // + "?imageName=";   // + Name=bild1, bild2,...
+		deletionLink     = "http://" + ip + ":4434/bootstrap/v1/images/" + userName + "?imageName="; // + "?imageName=";   // + Name=bild1, bild2,...
 		setMetaDataLink  = "http://" + ip + ":4434/bootstrap/v1/images/" + userName; // + "?imageName=";   // + $imageName/metadata"
         graphicsLink     = "http://" + ip + ":4434/bootstrap/v1/webclient/graphics/";
         logoutLink       = "http://" + ip + ":4434/bootstrap/v1/webclient/";
@@ -53,11 +53,14 @@
 	}
 
 	function getImageInfo(){
-
+		
 		if(!loggedIn){
-			userName = document.getElementById("userName").value
-			password = document.getElementById("password").value
+			userName = document.getElementById("userName").value;
+			password = document.getElementById("password").value;
 		}
+
+		userName = "user2";
+		password = "password";
 		
 
 		updateLinks();
@@ -81,8 +84,6 @@
 				json = JSON.parse(request.responseText);
                 loggedIn = true;
                 createNavi();
-                document.getElementById("LoginButton").innerHTML="Logout";
-                document.getElementById("LoginButton").setAttribute("onClick", "logout()");
                 createImages();
 			}
 		});
@@ -98,6 +99,7 @@
 			document.getElementById("userData").outerHTML="";
 		}
 
+		document.getElementById("userData").innerHTML = null;
 
 		var uploadButton = document.createElement("BUTTON");
 		
@@ -112,11 +114,20 @@
 		uploadInput.setAttribute("accept", "image/jpeg, image/png");
 		uploadInput.setAttribute("onchange", "uploadImage()");	
 		uploadInput.setAttribute("multiple", "true");	
-		
 		uploadButton.innerHTML = uploadInput.outerHTML + "Upload";
-		
 		document.getElementById("navigator").appendChild(uploadButton);
 
+		var deleteButton = document.createElement("BUTTON");
+		deleteButton.setAttribute("id", "delete");
+		deleteButton.setAttribute("onclick", "deleteMarkedImages()");
+		deleteButton.innerHTML = "Delete";
+		document.getElementById("navigator").appendChild(deleteButton);
+
+		var refreshButton = document.createElement("BUTTON");
+		refreshButton.setAttribute("id", "refresh");
+		refreshButton.setAttribute("onclick", "getImageInfo()");
+		refreshButton.innerHTML = "Refresh";
+		document.getElementById("navigator").appendChild(refreshButton);
 
 		var arrowLeft = document.createElement("IMG");
 		var arrowRight = document.createElement("IMG");
@@ -126,45 +137,20 @@
 		arrowRight.setAttribute("onClick", "goRight()");
 		arrowLeft.setAttribute("src", graphicsLink + "arrow_left.png");
 		arrowRight.setAttribute("src", graphicsLink + "arrow_right.png");
-		document.getElementById("navigator").appendChild(arrowRight);
-		document.getElementById("navigator").appendChild(arrowLeft);
+
+		var arrowDiv = document.createElement("DIV");
+		arrowDiv.setAttribute("id", "arrowDiv");
+		arrowDiv.appendChild(arrowLeft);
+		arrowDiv.appendChild(arrowRight);
+		document.getElementById("navigator").appendChild(arrowDiv);
+
+		document.getElementById("navigator").setAttribute("class", "navi sticky");
+
+		document.getElementById("LoginButton").innerHTML="Logout";
+		document.getElementById("LoginButton").setAttribute("onClick", "logout()");
+		document.getElementById("LoginButton").setAttribute("class", "logout");
+		
 	}
-
-	function uploadImage() {
-		var file = document.getElementById('upload-input').files[0];
-		var reader = new FileReader();
-		reader.readAsDataURL(file);
-
-		reader.onload = function () {
-			var baseToImageSource = reader.result.substring(23, reader.result.length);
-			var request = new XMLHttpRequest();
-			
-			request.open("POST", uploadLink);
-			request.setRequestHeader("Authorization", auth);
-			request.setRequestHeader("Content-Type", "application/json");
-
-
-
-			jsonString = {
-				"imageSource":baseToImageSource,
-				"imageName":"eduard232.jpg"
-			}
-
-			request.addEventListener('load', function(event) {
-				if (request.status != 201){
-					alert("Upload failed"+ request.status);
-				}
-				else{
-					alert("Upload successful :-)");
-					getImageInfo();
-				}
-			});
-			request.send(JSON.stringify(jsonString));
-			};
-
-	 }
-
-
 
 	function createImages(){
 
@@ -178,7 +164,7 @@
             document.getElementById("pictureDiv").innerHTML = null;
         }
 		
-		for(var i = 0  + page * 16; i < 16 * (page + 1) + page; i++) {
+		for(var i = 0  + page * 16; i < 16 * (page + 1); i++) {
 			if(json[i] == null){
 				break;
 			}
@@ -190,7 +176,8 @@
 		
 	}
 
-    function getThumbnailToUrl(linkToImage, i){
+
+	function getThumbnailToUrl(linkToImage, i){
         var request = new XMLHttpRequest();
 		
 		request.open("GET", linkToImage, true);
@@ -222,14 +209,107 @@
 		var imgTag = document.createElement("IMG");
 		imgTag.setAttribute("class", "picture");
 		imgTag.setAttribute("src", images[i].thumbnailBlobUrl);
-		imgTag.setAttribute("onClick", "setImageSource(" + i + ")");
+		imgTag.setAttribute("onClick", "markImage(" + i + ")");
+		imgTag.setAttribute("id", "img_" + i);
 		
 		var imageContainer = document.createElement("A");
 		imageContainer.innerHTML = imgTag.outerHTML;
 		document.getElementById("pictureDiv").appendChild(imageContainer);	
-
+		
 	}
-	
+
+
+	function uploadImage() {
+		var fileArray = document.getElementById('upload-input').files;
+
+		for(var i = 0; i < fileArray.length; i++){
+			readAndUpload(fileArray[i]);
+		}	
+	}
+
+
+	function readAndUpload(file) {
+		var name = file.name;
+
+		var reader = new FileReader();  
+		reader.onload = function (e) {
+
+			var baseToImageSource = e.target.result.substring(23, reader.result.length);
+			var request = new XMLHttpRequest();
+			
+			request.open("POST", uploadLink);
+			request.setRequestHeader("Authorization", auth);
+			request.setRequestHeader("Content-Type", "application/json");
+
+
+			jsonString = {
+				"imageSource":baseToImageSource,
+				"imageName":name 
+			}
+
+			request.addEventListener('load', function(event) {
+				if (request.status != 200){
+					console.log("Upload failed \nStatus Code: "+ request.status);
+				}
+				else{
+					console.log("Upload successful :-)");
+				}
+			});
+			request.send(JSON.stringify(jsonString));
+		};
+		reader.readAsDataURL(file);
+	}
+
+	function markImage(i){
+		var img = document.getElementById("img_" + i);
+		img.setAttribute("class", "picture marked");
+		img.setAttribute("onclick", "unmarkImage(" + i + ")");
+		img.setAttribute("value", i);
+	}
+
+	function unmarkImage(i){
+		var img = document.getElementById("img_" + i);
+		img.setAttribute("class", "picture");
+		img.setAttribute("onclick", "markImage(" + i + ")");
+		img.removeAttribute("value");
+	}
+
+	function deleteMarkedImages(){
+		var markedImages = document.getElementsByClassName("picture marked");
+		
+		var requestLink = deletionLink;
+		var queryString = "";
+
+		for(var i = 0; i < markedImages.length; i++){
+			var value = markedImages[i].getAttribute("value");
+			
+			queryString += images[value].imageName;
+
+			if(i != markedImages.length - 1){
+				queryString += ",";
+			}
+		}
+
+		requestLink += queryString;
+
+		console.log(requestLink);
+
+		var request = new XMLHttpRequest();
+			
+		request.open("DELETE", requestLink);
+		request.setRequestHeader("Authorization", auth);
+
+		request.addEventListener('load', function(event) {
+			if (request.status != 204){
+				console.log("Deletion failed\nStatus Code: "+ request.status);
+			}
+			else{
+				console.log("Deletion successful");
+				getImageInfo();
+			}
+		});
+		request.send();
+	}
 
 	function goLeft(){
         if(page==0){
@@ -247,12 +327,9 @@
         createImages();
 	}
 	
-
-	
-
-
     function logout(){
 		loggedIn = false;
+		page = 0;
         window.location.href = logoutLink;
 	}
 
